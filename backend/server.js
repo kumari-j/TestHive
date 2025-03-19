2d9db4d203300e85566aef998e800a8256965058
kumari-j <135324505+kumari-j@users.noreply.github.com>
2025-03-19 09:47:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get(/^\/(?!api).*/, (_, res)=>{
+    app.get('*',(_,res)=>{
     res.sendFile(path.resolve(__dirname,"../frontend","build","index.html"));
 });
 }
